20c3e5875c835bb5607fc3e6b5d3dc76b5f2684b
Evan Johnson <ecrjmegamanx@gmail.com>
2024-07-10 09:26:58 -0400

    first

---
+++ javascript/coaster_against_humanity/script.js
@@ -0,0 +1,11 @@
+let board=Array.from(document.getElementsByClassName("gridimage"));
+let act_array=[]
+let playstop=0
+
+for (let i=0;i<64;i++){
+    act_array.push(0)
+}
+
+while(playstop==0){
+    if 
+}
