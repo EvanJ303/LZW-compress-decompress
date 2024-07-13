2304e3bd4a1d00a58f1c1b7f73902f206b1d47ab
Evan Johnson <ecrjmegamanx@gmail.com>
2024-07-12 22:13:36 -0400

    fuck

--- python/web_scraper/gpu.py
+++ python/web_scraper/gpu.py
@@ -10,25 +10,25 @@ soup=BeautifulSoup(page.content, "html.parser")
 results=soup.find(id="contentResults")
 prod_sects=results.find_all("li", class_="product_wrapper")
 high_ends=results.find_all(
-    string=lambda text: "40" in text.lower()
+    string=lambda text: "4060" in text.lower()
 )
 prod_sects_parents=[
-    li_element.parent.parent.parent.parent.parent.parent for li_element in prod_sects
+    li_element.parent.parent.parent.parent.parent for li_element in prod_sects
 ]
 high_ends_parents=[
-    a_element.parent.parent.parent.parent.parent.parent for a_element in high_ends
+    a_element.parent.parent.parent.parent.parent for a_element in high_ends
 ]
 skip_count=0
 
-for job_parent in high_ends_parents:
-    links=job_parent.find_all("a")
+for high_end_parent in high_ends_parents:
+    links=high_end_parent.find_all("a")
     for link in links:
         l_url=link["href"]
         if skip_count==0:
-            webbrowser.open_new_tab(l_url)
+            #webbrowser.open_new_tab(l_url)
             print(f"BUY HERE: {l_url}\n")
         if skip_count==4:
             skip_count=0
         else:
             skip_count=skip_count+1
-        time.sleep(1.2)
+        #time.sleep(0.1)
